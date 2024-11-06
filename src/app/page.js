412854cb0b2a41d3bb2dc954d4591c60b5b5aba7
srinivasan-ad <forprogramingpurpose@gmail.com>
2024-11-06 18:27:47 +0530

    test commit

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,15 @@
 export default function Home() {
   const text  = "This is a test to check whether website is hosted on domain or not !";
   const text2 = "Nevertheless welcome to the website :)"
+  const text3 = "Tatti Mahan"
   return (
     <div className="flex h-screen w-full bg-slate-300 items-center justify-center">
       <div className="bg-blue-500 max-h-56 flex items-center justify-center p-10 text-xl">
         {text}
         <br/>
         {text2}
+        <br/>
+        {text3}
       </div>
      </div>
   );
