95b0d91eee23802be3441456ed188f986808128a
srinivasan-ad <forprogramingpurpose@gmail.com>
2024-11-10 16:00:00 +0530

    test commit

--- src/app/page.js
+++ src/app/page.js
@@ -6,9 +6,12 @@ export default function Home() {
   const text2 = "Nevertheless welcome to the website :)"
   return (
     <div className="flex h-screen w-full bg-slate-300 items-center justify-center">
+      <div className="bg-blue-500 max-h-56 flex items-center justify-center p-10 text-xl">
+
         {text}
         <br/>
         {text2}
+      </div>
      
     </div>
   );
